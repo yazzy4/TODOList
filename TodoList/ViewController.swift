fdda82f41d3672855f879c07b695fe3d43a38807
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-12-06 15:14:31 -0500

    adding files

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Yaz Burrell on 12/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("// TODO: Make a to do list")
+    }
+
+
+}
+
